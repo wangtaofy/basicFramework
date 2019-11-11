96a66e1a3a1fd73d93846879b1acd3a3d5accbe5
wangtaofy <13808909107@163.com>
2019-11-11 22:39:56 +0800

    添加element-ui、vant

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 按需引入element-ui
+import { Button } from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.prototype.$ELEMENT = { size: 'mini', zIndex: 3000 }
+Vue.use(Button)
 
 Vue.config.productionTip = false
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <el-button type="primary">button</el-button>
+    <Button size="mini">Button</Button>
   </div>
 </template>
+
+<script>
+import { Button } from 'vant'
+export default {
+  name: 'about',
+  components: {
+    Button
+  }
+}
+</script>
